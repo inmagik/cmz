3b02a679ec76ad6cc4751ad98c034bd1efaa63f4
Mauro Bianchi <bianchimro@gmail.com>
2016-05-08 22:36:36 +0200

    fixing paths in settings

--- cmz/cmz/settings.py
+++ cmz/cmz/settings.py
@@ -148,15 +148,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = CMZ_STATIC_PATH or '/static/'
-STATIC_ROOT = os.path.join(
+STATIC_ROOT = os.path.abspath(os.path.join(
     WEBSITE_PATH,
-    'build',
-    'html',
-    'static'
-)
+    'static',
+))
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, WEBSITE_PATH, 'website/static'),
+    os.path.join(BASE_DIR, WEBSITE_PATH, 'website', 'static'),
 ]
 
 
