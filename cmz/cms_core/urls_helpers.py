feb87be9fd5fa5c1fb82b00edebdd4abc8598d64
Mauro Bianchi <bianchimro@gmail.com>
2016-05-08 23:31:18 +0200

    fixed urls

--- cmz/cms_core/urls_helpers.py
+++ cmz/cms_core/urls_helpers.py
@@ -5,17 +5,33 @@ from .views import CmsView
 
 def create_urls(pages):
     out = []
+    empty_urls = []
     for page_name in pages:
         
         page = pages[page_name]
         extra_modules = page.get('extra_modules', [])
-        comp_url = r'%s' % page['url']
+        if page['url']:
+            comp_url = r'%s/$' % page['url']
+        else:
+            comp_url = r''
+
         u = url(comp_url, CmsView.as_view(
                 page_name=page_name, extra_modules=extra_modules
             ), name=page_name
         )
-        out.append(u)
+        
+        if page['url']:
+            out.append(u)
+        else:
+            empty_urls.append(u)
 
+        if len(empty_urls) > 2:
+            raise ValueError("CMZ ERROR: your pages.py has more than one empty url")
+
+
+    #trick for allowing ''
+    out.extend(empty_urls)
+        
     return out
 
     
