1675cbf4d97f74652e2c345cb1227dfd710e0592
Mauro Bianchi <bianchimro@gmail.com>
2016-09-14 23:29:20 +0200

    removed medusa references

--- cmz/cms_core/renderers.py
+++ cmz/cms_core/renderers.py
@@ -1,15 +1,7 @@
-from django_medusa.renderers import StaticSiteRenderer
 from django.conf import settings
 from .urls_helpers import create_urls
 SITE_PAGES = getattr(settings, 'SITE_PAGES')
 
 urls = [ u._regex for u in create_urls(SITE_PAGES) ]
-print urls
 
-class CMSRenderer(StaticSiteRenderer):
-    def get_paths(self):
-        return frozenset([
-            "/"
-        ])
-
-renderers = [CMSRenderer, ]
+renderers = []
