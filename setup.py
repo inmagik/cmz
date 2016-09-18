b4d66e484e4aadad532953dbee242e731b31cd50
Mauro Bianchi <bianchimro@gmail.com>
2016-09-18 22:23:42 +0200

    dynamic requirements

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@
 import sys
 from setuptools import setup
 from setuptools import find_packages
+import os
+
+with open("requirements.txt", "rb") as requirements:
+    lines = requirements.readlines()
+    install_requires = [x.replace("\n", "") for x in lines]
 
 
 
@@ -16,8 +21,7 @@ setup(name='cmz',
       package_dir={'':'cmz'},
       include_package_data=True,
       scripts = ["cmz/bin/cmz_play.py", "cmz/bin/cmz_start.py" , "cmz/bin/cmz_manage.py", "cmz/manage.py" ],
-      install_requires=['Django==1.9.5',
-      'django-classy-tags==0.7.2', 'django-sekizai==0.9.0', 'wheel==0.26.0'],
+      install_requires=install_requires,
 
       #dependency_links=['git+https://github.com/gbozee/django-activelink.git@c31619ceb0fe16d96adae524fb44a8b135d85597#egg=django_activelink']
     )
