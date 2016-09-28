de7ffb372372746918be39f62f9bccee1ac6b9e4
Mauro Bianchi <bianchimro@gmail.com>
2016-09-28 16:18:08 +0200

    fixed requirements, cmz_translation tag with default

--- cmz/cmz_translations/templatetags/cmz_translations.py
+++ cmz/cmz_translations/templatetags/cmz_translations.py
@@ -5,11 +5,13 @@ from django.utils.html import mark_safe
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
-def cmz_translation(context, label, page_context=None):
+def cmz_translation(context, label, page_context=None, default=None):
     page = context['request'].GET.get('page', 1);
     page = int(page)
+    if not page_context:
+        page_context = None
     try:
         instance = TranslatedText.objects.language().get(label=label, context=page_context)
         return mark_safe(instance.body)
     except TranslatedText.DoesNotExist:
-        return "Not translated"
+        return default or "Not translated"
