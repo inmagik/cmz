769cab96f832fc98d6e116dbe10ff92610e32f9d
Mauro Bianchi <bianchimro@gmail.com>
2016-09-28 15:12:29 +0200

    restored sys.path.append, changed environment var in cmz_manage

--- cmz/bin/cmz_manage.py
+++ cmz/bin/cmz_manage.py
@@ -7,5 +7,6 @@ if __name__ == "__main__":
     env = { "CMZ_WEBSITE_PATH" : os.getcwd() }
     env.update(os.environ)
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cmz.settings")
+    os.environ.setdefault("CMZ_WEBSITE_PATH", os.getcwd() )
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
