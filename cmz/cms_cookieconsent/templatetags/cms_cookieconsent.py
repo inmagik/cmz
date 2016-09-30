b4db0a5f5e8aefd474191236f110404dbdf6cbd9
Mauro Bianchi <bianchimro@gmail.com>
2016-09-30 16:48:45 +0200

    cookieconsent with resolve

--- cmz/cms_cookieconsent/templatetags/cms_cookieconsent.py
+++ cmz/cms_cookieconsent/templatetags/cms_cookieconsent.py
@@ -1,11 +1,20 @@
 from django import template
 from django.conf import settings
+from django.core.urlresolvers import reverse
+from django.core.urlresolvers import NoReverseMatch
 import json
 
 register = template.Library()
 COOKIECONSENT_OPTIONS = getattr(settings, "COOKIECONSENT_OPTIONS", {})
 if type(COOKIECONSENT_OPTIONS) != dict:
     raise TypeError("COOKIECONSENT_OPTIONS must be a dict")
+
+if 'link' in COOKIECONSENT_OPTIONS and not COOKIECONSENT_OPTIONS['link'].startswith("/"):
+    try:
+        COOKIECONSENT_OPTIONS['link'] = reverse(COOKIECONSENT_OPTIONS['link'])
+        print COOKIECONSENT_OPTIONS['link']
+    except NoReverseMatch:
+        pass
 
 #TODO: max_len is useless here, should be moved to model somehow
 @register.inclusion_tag('cms_cookieconsent/cookieconsent_script.html')
